859a0a044e0ad3d0c33ff1943e87f61c6e833a3b
Yoga Ananda Prasetya <kuriose.97@gmail.com>
2025-11-26 02:47:42 +0800

    feat: Pengerjaan Fitur Buat Laporan Keluhan untuk User

--- source-code/user/menuKeluhan.py
+++ source-code/user/menuKeluhan.py
@@ -1,15 +1,161 @@
+import inquirer
+
+dataPenyewa = {
+    "PENYEWA1": {
+        "nama": "Pirlo", 
+        "kontak": "0XXX-XXXX-XXXX",
+        "tanggal_gabung": "2 November 2025", 
+        "status": "AKTIF", 
+        "unit": "KS01", 
+        "kamar": "A1"
+        }, 
+    "PENYEWA2": {
+        "nama": "Yoga", 
+        "kontak": "0XXX-XXXX-XXXX",
+        "tanggal_gabung": "10 November 2025", 
+        "status": "AKTIF", 
+        "unit": "KN01", 
+        "kamar": "-"
+    }
+}
+
+laporan_keluhan = {
+    "PENYEWA1": {
+        "LK-1": {
+            "nama": "",
+            "unit": "",
+            "kamar": "", 
+            "kategori": "", 
+            "judul_laporan": "", 
+            "deskripsi_laporan": "", 
+            "tanggal_dibuat": "", 
+            "status": ""
+        }
+    }
+}
+
+laporan_bayar = {
+    "PENYEWA1": {
+        "LB-1": {
+            "nama": "", 
+            "unit": "", 
+            "kamar": "", 
+            "jumlah_periode": "", 
+            "periode": ["", ""], 
+            "jumlah_pembayaran": 0, 
+            "metode_pembayaran": ["1", "123"]
+        }
+    },
+    "PENYEWA2": {
+    }
+}
+
 # Menu Keluhan 
 from fungsi.utilitas import clear
 
 # tempat nyimpan sementara aku masih belum tau mau dipakai buat apa
-laporan_keluhan = []
+# laporan_keluhan = []
+
+def deskripsi_laporan(prompt, end_keyword="SELESAI"):
+    print(f"{prompt}")
+    print(f"(Ketik '{end_keyword}' di baris baru untuk selesai.)")
+
+    lines = []
+    while True:
+        line = input()
+        if line.strip().upper() == end_keyword:
+            break
+        lines.append(line)
+    
+    hasil = "\n".join(lines).strip()
+    
+    if hasil == "":
+        print("Deskripsi tidak boleh kosong. Coba lagi.\n")
+        return deskripsi_laporan(prompt, end_keyword)
+
+    return hasil
+
 
 # function fitur masih kosong
 def buat_laporan_keluhan():
     clear()
-    print("=" * 75)
+    # print("=" * 75)
     print("BUAT LAPORAN KELUHAN")
     print("=" * 75)
+
+    pilih_id = input("Masukkan ID Penyewa : ")
+
+    for id_penyewa, info_penyewa in dataPenyewa.items():
+        if pilih_id == id_penyewa: 
+            nama_laporan = info_penyewa['nama']
+            unit_laporan = info_penyewa['unit']
+            kamar_laporan = info_penyewa['kamar']
+
+            print("Nama Penyewa :", nama_laporan)
+            print("Unit :", unit_laporan)
+            print("Kamar :", kamar_laporan)
+            print("")
+
+            tanggal_laporan = input("Masukkan Tanggal dibuat : ")
+            print("KATEGORI LAPORAN")
+            print("[1] - Fasilitas Rusak")
+            print("[2] - Kebersihan")
+            print("[3] - Keamanan")
+            print("[4] - Gangguan")
+            print("[5] - Lainnya")
+
+            print("Masukkan Kategori laporan anda : ")
+            pilih_menu = input("> ")
+
+            if pilih_menu == "1": 
+                kategori_laporan = "Fasilitas Rusak"
+            elif pilih_menu == "2": 
+                kategori_laporan = "Kebersihan"
+            elif pilih_menu == "3": 
+                kategori_laporan = "Keamanan"
+            elif pilih_menu == "4": 
+                kategori_laporan = "Gangguan Lingkungan"
+            elif pilih_menu == "5": 
+                kategori_laporan = "Lainnya"
+            else: 
+                print("Pilihan Tidak valid!")
+
+            judul_laporan = input("Masukkan Judul Laporan : ")
+            deskripsi = deskripsi_laporan("Tulis deskripsi Keluhan:")
+            print("\nDeskripsi final:")
+            
+            for id_penyewa, data_laporan in laporan_keluhan.items(): 
+                print(laporan_keluhan)
+                if pilih_id == id_penyewa: 
+                    id_laporan = f"LK-{len(data_laporan) + 1}"
+                    break
+
+            for id_penyewa, data_laporan in laporan_keluhan.items(): 
+                if id_penyewa == pilih_id: 
+                    print("BERHASIL")
+                    data_laporan[id_laporan] = {
+                        "nama": nama_laporan, 
+                        "unit": unit_laporan, 
+                        "kamar": kamar_laporan, 
+                        "kategori": kategori_laporan, 
+                        "judul_laporan": judul_laporan, 
+                        "deskripsi_laporan": deskripsi,
+                        "tanggal_dibuat": tanggal_laporan, 
+                        "status": "BELUM DITANGANI"
+                    }
+            
+            # laporan_keluhan.append(deskripsi)
+            # for i in laporan_keluhan: 
+            #     print(i)
+            print(laporan_keluhan)
+            unloading = laporan_keluhan["PENYEWA1"][id_laporan]["deskripsi_laporan"]
+
+            print(unloading)
+            # for baris in unloading: 
+            #     print(baris)
+
+            # print(info_penyewa['nama']) 
+
     input("Tekan Enter untuk kembali...")
 
 # fungsi liat laporan keluhan yang udah dibuat
@@ -49,7 +195,7 @@ def keluhan():
         print("[0] - Kembali ke Menu Utama")
         print("=" * 75)
 
-        pilih = input("> ").strip()
+        pilih = input("> ")
 
         if pilih == "0":
             break
